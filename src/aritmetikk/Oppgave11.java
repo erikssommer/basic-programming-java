6b70c18c4dc1555d7c662018bbe1e5179b98635d
Erik Storås Sommer <eriksommer99@gmail.com>
2020-09-10 11:40:23 +0200

    Alternativ løsning til å finne tverrsummen av et tall

--- src/aritmetikk/Oppgave11.java
+++ src/aritmetikk/Oppgave11.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 public class Oppgave11 {
     public static void main(String[] args) {
+
+        //Alternativ 1 (Slik det står i løsningsforslaget gitt)
+        /*
         int tall = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn tallet: "));
         int utTall = tall;
         int sum = 0;
@@ -19,5 +22,20 @@ public class Oppgave11 {
         String melding = "Tverrsummen av tallet " + utTall + " er " + sum;
 
         System.out.println(melding);
+
+         */
+
+        //Alternativ 2 (I min mening en litt mer enkel løsning å forstå og utføre)
+        int innTall = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn tallet: "));
+        int tall1, tall2, tall3 = 0;
+
+        tall1 = innTall / 100;
+        tall2 = (innTall % 100) / 10;
+        tall3 = innTall % 10;
+
+        int tverrsum = tall1 +tall2 + tall3;
+
+        System.out.println("Tverrsummen av " + innTall + " er " + tverrsum);
+
     }
 }
