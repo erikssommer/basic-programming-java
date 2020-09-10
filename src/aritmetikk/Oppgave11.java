15a18990b609ee9b787f522d65238d03d7a7571d
Erik Storås Sommer <eriksommer99@gmail.com>
2020-09-10 14:33:30 +0200

    Lagt til forklaring

--- src/aritmetikk/Oppgave11.java
+++ src/aritmetikk/Oppgave11.java
@@ -27,11 +27,11 @@ public class Oppgave11 {
 
         //Alternativ 2 (I min mening en litt mer enkel løsning å forstå og utføre)
         int innTall = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn tallet: "));
-        int tall1, tall2, tall3 = 0;
+        int tall1, tall2, tall3;
 
-        tall1 = innTall / 100;
-        tall2 = (innTall % 100) / 10;
-        tall3 = innTall % 10;
+        tall1 = innTall / 100;        // --> 7,51 = 7
+        tall2 = (innTall % 100) / 10; // --> (51) / 10 = 5,1 = 5
+        tall3 = innTall % 10;         // --> 1
 
         int tverrsum = tall1 + tall2 + tall3;
 
