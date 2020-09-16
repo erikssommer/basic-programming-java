7bf64947c1eee221b11fb55a7356dc2882ca8cfd
Erik Storås Sommer <eriksommer99@gmail.com>
2020-09-16 11:26:27 +0200

    Løsningforslag til introduksjon til klasser og objekter 1

--- src/introklasserogobjekter1/Oppgave5.java
+++ src/introklasserogobjekter1/Oppgave5.java
@@ -8,11 +8,11 @@ public class Oppgave5 {
         int tall1 = 2;
         int tall2 = 3;
 
-        DecimalFormat df = new DecimalFormat("#0.00");
+        DecimalFormat df = new DecimalFormat("#0.00"); //Ikke spurt om i oppgaven
 
-        System.out.println("3 + 1 = " + Kalkulator.addere(tall1, tall2));
-        System.out.println("3 - 1 = " + Kalkulator.subtrahere(tall1, tall2));
-        System.out.println("3 * 1 = " + Kalkulator.multiplisere(tall1, tall2));
-        System.out.println("3 / 1 = " + df.format(Kalkulator.dividere(tall1, tall2)));
+        System.out.println(tall1 + " + " + tall2 + " = " + Kalkulator.addere(tall1, tall2));
+        System.out.println(tall1 + " - " + tall2 + " = " + Kalkulator.subtrahere(tall1, tall2));
+        System.out.println(tall1 + " * " + tall2 + " = " + Kalkulator.multiplisere(tall1, tall2));
+        System.out.println(tall1 + " / " + tall2 + " = " + df.format(Kalkulator.dividere(tall1, tall2)));
     }
 }
