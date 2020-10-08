887e4eb65950b39e1ccaf4e37507d7a1d097e50b
Erik Storås Sommer <eriksommer99@gmail.com>
2020-10-08 15:16:48 +0200

    Øving uke 41

---
+++ src/oving41/Annet.java
@@ -0,0 +1,12 @@
+package oving41;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Annet {
+    public static void main(String[] args) {
+        int[] liste = {3,4,3,12};
+
+        System.out.println(Arrays.toString(liste));
+    }
+}
